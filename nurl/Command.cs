fa199cead8fd1b9242fa478089214856b1cf575e
mfiari <fiarimike@yahoo.fr>
2014-05-28 14:41:48 +0200

    creation de la class command avec la methode show et du test vérifiant cette méthode

---
+++ nurl/Command.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mike
+ * Date: 28/05/2014
+ * Time: 14:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace nurl
+{
+	/// <summary>
+	/// Description of Command.
+	/// </summary>
+	public class Command {
+		
+		public Command() {
+			
+		}
+		
+		public string show (string url) {
+			return "";
+		}
+ 		
+	}
+}
---
+++ nurl/Test.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mike
+ * Date: 28/05/2014
+ * Time: 14:20
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace nurl {
+	
+	[TestFixture]
+	public class Test {
+		
+		[Test]
+		public void TestShow() {
+			Command commande = new Command ();
+			var url = "http://fake";
+			var result = commande.show(url);
+			//Assert.Equals("", result);
+			Assert.AreSame("", result, "not same", null);
+		}
+	}
+}
