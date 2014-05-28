51264de6205c6b9386032248f1fd19476447259c
mfiari <fiarimike@yahoo.fr>
2014-05-28 14:09:57 +0200

    creation du projet csharp

---
+++ nurl/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mike
+ * Date: 27/05/2014
+ * Time: 19:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace nurl
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
