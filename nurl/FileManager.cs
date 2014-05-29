094d5279c92f30bd551d2085f0797d3ea6de916e
mfiari <fiarimike@yahoo.fr>
2014-05-29 11:42:48 +0200

    creation de la class FileManager et du fichier test correspondant

---
+++ nurl/FileManager.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mike
+ * Date: 28/05/2014
+ * Time: 21:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace nurl
+{
+	/// <summary>
+	/// Description of FileManager.
+	/// </summary>
+	public class FileManager
+	{
+		public FileManager()
+		{
+		}
+	}
+}
---
+++ nurl/FileManagerTest.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mike
+ * Date: 29/05/2014
+ * Time: 11:41
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace nurl
+{
+	[TestFixture]
+	public class FileManagerTest
+	{
+		[Test]
+		public void TestMethod()
+		{
+			// TODO: Add your test.
+		}
+	}
+}
